105df98974720a445f714577dcbebfed6a96ac6a
danamr05 <danamtzreynoso@gmail.com>
2023-02-22 10:09:53 -0600

    Agregamos fecha dinamica

--- src/componentes/Encabezado.jsx
+++ src/componentes/Encabezado.jsx
@@ -1,12 +1,14 @@
 import Titulo from "./Titulo";
 
 function Encabezado (props) {
+    let fecha = new Intl.DateTimeFormat('es-MX', { dateStyle: 'full', timeZone: 'America/Mexico_City' }).format(new Date());
+    fecha = fecha.charAt(0).toUpperCase() + fecha.slice(1);
     return (
         <>
-        <Titulo mensaje="Martes 21 de Enero 2023"/>
+        <Titulo mensaje={fecha}/>
         <Titulo subtitulo mensaje={`${props.pins.lenght} Pins`} />
         </>
     )
 }
 
-export default Encabezado;
+export default Encabezado; 
